6064198119ae1c86324b1baa8ba75a61612854c1
thymac <thywillberdone@gmail.com>
2022-10-17 11:32:10 -0700

    revised

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,14 +4,13 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- *     */
+ */
 
 int main(void)
 
 {
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 
 }
