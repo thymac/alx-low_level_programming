c6c813b9dcf923ee6b439ef5213efab70e60c008
thymac <thywillberdone@gmail.com>
2022-10-18 03:23:49 -0700

    revised

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha, e, q;
+	e = 'e';
+	q = 'q';
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != e && alpha != q)
+			putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
