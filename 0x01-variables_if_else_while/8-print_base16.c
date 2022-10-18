0ee486eb41e2f1f78cd1a33c2e26365b0d741dca
thymac <thywillberdone@gmail.com>
2022-10-18 04:49:47 -0700

    revised

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	int n;
 	char alpha;
 
-	for (alpha = 'z'; alpha >= 'a'; alpha--)
-		putchar(alpha);
+	for (n = 0; n <= 9; n++)
+		putchar(n);
+	for (alpha = 'a'; alpha <= 'f'; aplha++)
+		putchar(alpha');
 	putchar('\n');
 
 	return(0);
