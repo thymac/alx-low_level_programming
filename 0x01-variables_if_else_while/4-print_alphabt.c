73105a4695aa2d022acf507834d65393ee213bf6
thymac <thywillberdone@gmail.com>
2022-10-18 03:47:09 -0700

    revised

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,11 +11,13 @@ int main(void)
 	char alpha, e, q;
 	e = 'e';
 	q = 'q';
+
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		if (alpha != e && alpha != q)
 			putchar(alpha);
 	}
 	putchar('\n');
+
 	return (0);
 }
