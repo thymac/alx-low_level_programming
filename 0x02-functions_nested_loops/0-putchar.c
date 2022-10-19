b01781ae26d664fbb0a53bad27be5cc334dc0c8a
thymac <thywillberdone@gmail.com>
2022-10-19 09:14:39 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "_putchar.h"
+/**
+ * main -Entry point
+ *
+ * Result: Always 0 (Success)
+ */
+int main(void)
+{
+	char wrd[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+		_putchar(wrd[i]);
+	_putchar('\n');
+
+	return (0);
+}
