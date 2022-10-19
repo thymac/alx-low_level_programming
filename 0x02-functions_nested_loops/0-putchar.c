6e26a062f1f40d11318335c6d562e74cf1cb5d35
thymac <thywillberdone@gmail.com>
2022-10-19 09:25:04 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include "_putchar.h"
 /**
- * main -Entry point
+ * main - Entry point
  *
  * Result: Always 0 (Success)
  */
 int main(void)
 {
-	char wrd[8] = "_putchar";
+	char word[8] = "_putchar";
 	int i;
 
 	for (i = 0; i < 8; i++)
-		_putchar(wrd[i]);
+		_putchar(word[i]);
 	_putchar('\n');
 
 	return (0);
