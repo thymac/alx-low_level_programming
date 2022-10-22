4cb148e988013b904b1fce78426fd15841308bb5
thymac <thywillberdone@gmail.com>
2022-10-22 08:19:46 -0700

    revised

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ * @i: An integer input
+ * @j: An integer input
+ *
+ * Return: Result of adding i and j
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Prints all neutral numbers from n to 98
+ * @n: An input integer
+ *
+ * Return: Nothing
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(",");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(",");
+		}
+		printf("\n");
+	}
+}
