a0a46c9d7b5da52ad8c53255800b9e0fdfd9fffe
thymac <thywillberdone@gmail.com>
2023-03-26 02:27:16 -0700

    revised

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,41 +1,32 @@
 #!/usr/bin/python3
-""" This module computes the perimeter of an island"""
-
+"""
+Island Perimeter
+"""
 
 def island_perimeter(grid):
-	""" island_perimeter:
-		Args: grid, a list of list of integers
-		Return: perimeter of the island described in grid
-	"""
+    """returns the perimeter of the island described in grid
 
-    perimeter = 0
-    env_list = []
+    Args:
+        grid(matrix): input grid
 
-    for items in range(len(grid)):
-        if items == 0 or items == len(grid):
-            continue
-        for cell in range(len(grid[items])):
-            if cell == 0 or cell == len(grid[items]):
-                continue
-            else:
-                if grid[items][cell] == 1:
-                    env_list = [grid[items - 1][cell], grid[items][cell + 1],
-                                grid[items + 1][cell],  grid[items][cell - 1]]
-                    count = 0
-                    for j in env_list:
-                        if j == 0:
-                            count = count + 1
-                    if (count == 4):
-                        return 4
-                    elif (count == 3):
-                        perimeter = perimeter + 3
-                    elif (count == 2):
-                        perimeter = perimeter + 2
-                    elif (count == 1):
-                        perimeter = perimeter + 1
-                    else:
-                        pass
-                    env_list = []
-                else:
-                    pass
-    return perimeter
+    Description:
+        Traverse the land mass, and
+        for the lake's nearest neighbor
+        then return the total length
+
+    """
+    y = 0
+    x = 0
+    
+    for row in range(1, len(grid) - 1):
+            for col in range(1, len(grid[row]) - 1):
+                if grid[row][col] == 1:
+                    if grid[row][col - 1] == 0:
+                        y += 1
+                    if grid[row][col + 1] == 0:
+                        y += 1
+                    if grid[row - 1][col] == 0:
+                        x += 1
+                    if grid[row + 1][col] == 0:
+                        x += 1
+    return x + y
